c2ea313740799e13e8efc911484806799f59b85a
emaliemcmahon@gmail.com <emaliemcmahon@gmail.com>
2022-02-28 14:01:36 -0500

    reorg

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import find_packages, setup
 
-setup(name='src',
-      version='0.1',
-      packages=find_packages())
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Code need to reproduce analyses in McMahon et al 2022',
+    author='Emalie McMahon',
+    license='MIT',
+)
