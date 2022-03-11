deffd4b88caa47c585baad4d1643f7093ce467b3
emaliemcmahon@gmail.com <emaliemcmahon@gmail.com>
2022-03-11 11:14:03 -0500

    marcc permutation

--- scripts/swurm_permutation.sh
+++ scripts/swurm_permutation.sh
@@ -1,16 +1,20 @@
 features=( "indoor" "expanse" "agent_distance" "facingness" "transitivity" "joint_action" "communication" "cooperation" "dominance" "intimacy" "valence" "arousal" )
-for feature in "${feature[@]}"; done
+for feature in "${features[@]}"; do
   for sid in $(seq 1 4); do
-    echo sbatch_permutation.sh "${sid}" by_feature
-    sbatch sbatch_permutation.sh "${sid}" by_feature
+    echo sbatch_permutation.sh "${sid}" "${feature}"
+    sbatch sbatch_permutation.sh "${sid}" "${feature}"
   done
 done
 
-sbatch sbatch_permutation.sh all by_feature
-
-for sid in $(seq 1 4); do
-  echo sbatch_permutation.sh "${sid}"
-  sbatch sbatch_permutation.sh "${sid}" not_by_feature
+for feature in "${features[@]}"; do
+  echo sbatch_permutation.sh "all" "${feature}"
+  sbatch sbatch_permutation.sh "all" "${feature}"
 done
 
-sbatch sbatch_permutation.sh all not_by_feature
+for sid in $(seq 1 4); do
+  echo sbatch_permutation.sh "${sid}" "not_by_feature"
+  sbatch sbatch_permutation.sh "${sid}" "not_by_feature"
+done
+
+echo sbatch_permutation.sh "all" "not_by_feature"
+#sbatch sbatch_permutation.sh "all" "not_by_feature"
