09f6756161b0441e292c96eded9c641c04c50921
emaliemcmahon@gmail.com <emaliemcmahon@gmail.com>
2022-03-11 11:09:31 -0500

    marcc permutation

--- scripts/sbatch_permutation.sh
+++ scripts/sbatch_permutation.sh
@@ -7,9 +7,18 @@
 #SBATCH --mail-type=end
 
 sid=$1
+feature=$2
 
 ml anaconda
 conda activate nibabel
-python voxel_permutation.py -s $sid \
--data /home-2/emcmaho7@jhu.edu/work/mcmahoneg/SIfMRI_analysis/data/raw \
--output /home-2/emcmaho7@jhu.edu/work/mcmahoneg/SIfMRI_analysis/data/interim
+if [ "$feature" = "not_by_feature" ]; then
+  python voxel_permutation.py -s $sid \
+  -data /home-2/emcmaho7@jhu.edu/work/mcmahoneg/SIfMRI_analysis/data/raw \
+  -output /home-2/emcmaho7@jhu.edu/work/mcmahoneg/SIfMRI_analysis/data/interim
+else
+  python voxel_permutation.py -s $sid \
+  -data /home-2/emcmaho7@jhu.edu/work/mcmahoneg/SIfMRI_analysis/data/raw \
+  -output /home-2/emcmaho7@jhu.edu/work/mcmahoneg/SIfMRI_analysis/data/interim \
+  --by_feature \
+  --feature "$feature"
+fi
--- scripts/swurm_permutation.sh
+++ scripts/swurm_permutation.sh
@@ -1,5 +1,16 @@
-#features=( "indoor" "expanse" "agent_distance" "facingness" "transitivity" "joint_action" "communication" "cooperation" "dominance" "intimacy" "valence" "arousal" )
+features=( "indoor" "expanse" "agent_distance" "facingness" "transitivity" "joint_action" "communication" "cooperation" "dominance" "intimacy" "valence" "arousal" )
+for feature in "${feature[@]}"; done
+  for sid in $(seq 1 4); do
+    echo sbatch_permutation.sh "${sid}" by_feature
+    sbatch sbatch_permutation.sh "${sid}" by_feature
+  done
+done
+
+sbatch sbatch_permutation.sh all by_feature
+
 for sid in $(seq 1 4); do
   echo sbatch_permutation.sh "${sid}"
-  sbatch sbatch_permutation.sh "${sid}"
+  sbatch sbatch_permutation.sh "${sid}" not_by_feature
 done
+
+sbatch sbatch_permutation.sh all not_by_feature
