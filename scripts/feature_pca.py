0b7f178f8140e81972e69888b6b6def27ea28427
emaliemcmahon@gmail.com <emaliemcmahon@gmail.com>
2022-03-23 09:56:15 -0400

    update feature_pca.py

--- scripts/feature_pca.py
+++ scripts/feature_pca.py
@@ -3,57 +3,81 @@
 
 import argparse
 import os
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 from sklearn.decomposition import PCA
+from scipy.stats import spearmanr
 import matplotlib.pyplot as plt
+import seaborn as sns
+from src.custom_plotting import custom_seaborn_cmap, feature_categories
 
 
-def pca(X):
-    model = PCA(whiten=True)
-    model.fit(X)
-    return model.components_, model.explained_variance_ratio_
+def plot_feature_correlation(cur, ax):
+    cmap = custom_seaborn_cmap()
+    cur = cur.sort_values(by='Spearman rho', ascending=False)
+    sns.barplot(y='Spearman rho', x='Feature',
+                hue='category',
+                data=cur, ax=ax,
+                palette=cmap,
+                dodge=False)
+    plt.title('')
+    ax.tick_params(axis='x', labelrotation=90)
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.set_xlabel('')
+    legend = ax.legend()
+    legend.remove()
+
+
+def plot_video_loadings(loading, videos, ax):
+    indices = np.flip(np.argsort(loading))
+    df = pd.DataFrame({'Videos': videos[indices],
+                       'Loading': loading[indices]})
+    sns.barplot(y='Loading', x='Videos',
+                data=df, ax=ax, color='gray',
+                dodge=False)
+    plt.title('')
+    ax.tick_params(axis='x', labelrotation=90, labelsize=12)
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.set_xlabel('')
+    legend = ax.legend()
+    legend.remove()
+
+
+def pca(X, n_components):
+    model = PCA(whiten=True, n_components=n_components)
+    out = model.fit_transform(X)
+    return out, model.components_, model.explained_variance_ratio_
 
 
 class FeaturePCA():
     def __init__(self, args):
         self.process = 'FeaturePCA'
+        self.set = args.set
         self.data_dir = args.data_dir
-        self.out_dir = f'{args.out_dir}/{self.process}'
+        self.out_dir = args.out_dir
         self.figure_dir = f'{args.figure_dir}/{self.process}'
-        if not os.path.exists(self.out_dir):
-            os.mkdir(self.out_dir)
+        if not os.path.exists(f'{self.out_dir}/{self.process}'):
+            os.mkdir(f'{self.out_dir}/{self.process}')
         if not os.path.exists(self.figure_dir):
             os.mkdir(self.figure_dir)
+        self.n_components = args.n_components
+        if self.n_components > 1:
+            self.n_components = int(self.n_components)
 
     def load_features(self):
         df = pd.read_csv(f'{self.data_dir}/annotations/annotations.csv')
-        train = pd.read_csv(f'{self.data_dir}/annotations/train.csv')
+        train = pd.read_csv(f'{self.data_dir}/annotations/{self.set}.csv')
         df = df.merge(train)
         df.sort_values(by=['video_name'], inplace=True)
-        df.drop(columns=['video_name'], inplace=True)
-        feature_names = np.array(df.columns)
-        return feature_names, df.to_numpy()
+        new = df.drop(columns=['video_name'])
 
-    def summarize_results(self, components, explained_variance, feature_names):
-        df = pd.DataFrame()
-        n_PCs = components.shape[0]
-        n_features = components.shape[1]
-        for iPC in range(n_PCs):
-            indices = np.flip(np.argsort(components[iPC, :]))
-            ordered_features = feature_names[indices]
-            ordered_components = components[iPC, indices]
-            d = {'Explained variance': [explained_variance[iPC]]}
-            for ifeature in range(n_features):
-                d[f'Feature{str(ifeature+1).zfill(2)}'] = ordered_features[ifeature]
-            for ifeature in range(n_features):
-                d[f'Feature{str(ifeature+1).zfill(2)}_weight'] = ordered_components[ifeature]
-            df = pd.concat([df, pd.DataFrame(d)])
-        return df.reset_index(drop=True)
+        categories = pd.read_csv(f'{self.data_dir}/annotations/{self.set}_categories.csv')
+        return np.array(new.columns), new, categories.action_categories.to_numpy()
 
-    def plot(self, vals):
-        print()
+    def plot_variance(self, vals):
         fig, ax = plt.subplots()
         ax.plot(vals, '.-r')
         ax.plot(np.ones(len(vals))*0.8, 'k', alpha=0.3)
@@ -63,21 +87,54 @@ class FeaturePCA():
         plt.ylim([0, 1.05])
         plt.xlabel('PCs')
         plt.ylabel('Explained variance')
-        plt.savefig(f'{self.figure_dir}/explained_variance.pdf')
+        plt.savefig(f'{self.figure_dir}/explained_variance_set-{self.set}.pdf')
+
+    def PC_to_features(self, features, feature_names, vid_comp):
+        categories = feature_categories()
+        df = pd.DataFrame()
+        for iPC in range(vid_comp.shape[-1]):
+            d = dict()
+            for feature in feature_names:
+                rho, _ = spearmanr(features[feature], vid_comp[:, iPC])
+                d['Feature'] = feature
+                d['Spearman rho'] = rho
+                d['PC'] = [iPC]
+                d['category'] = categories[feature]
+                df = pd.concat([df, pd.DataFrame(d)])
+        df.category = pd.Categorical(df.category,
+                              categories=['scene', 'object', 'social primitive', 'social'],
+                              ordered=True)
+        df.to_csv(f'{self.out_dir}/{self.process}/PCs_set-{self.set}.csv', index=False)
+        return df
+
+    def plot_PC_results(self, df, videos, vid_comp):
+        sns.set(style='whitegrid', context='talk')
+        for i, iname in enumerate(np.unique(df.PC)):
+            _, ax = plt.subplots(1, 2, figsize=(18, 9), gridspec_kw={'width_ratios': [1, 1.5]})
+            plot_feature_correlation(df[df['PC'] == iname], ax[0])
+            plot_video_loadings(vid_comp[:, i], videos, ax[1])
+            plt.xticks(rotation=90)
+            plt.tight_layout()
+            plt.savefig(f'{self.figure_dir}/PC{str(i).zfill(2)}_set-{self.set}.pdf')
+            plt.close()
 
     def run(self):
-        feature_names, X = self.load_features()
-        components, explained_variance = pca(X)
-        self.plot(explained_variance.cumsum())
-        df = self.summarize_results(components, explained_variance, feature_names)
-        print(df.head())
-        df.to_csv(f'{self.out_dir}/PCs.csv', index=False)
+        feature_names, features, videos = self.load_features()
+        print(features.head())
+        vid_comp, comp_feature, explained_variance = pca(features, self.n_components)
+        self.plot_variance(explained_variance.cumsum())
+        df = self.PC_to_features(features, feature_names, vid_comp)
+        self.plot_PC_results(df, videos, vid_comp)
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_dir', '-data', type=str, default='/Users/emcmaho7/Dropbox/projects/SI_fmri/SIfMRI_analysis/data/raw')
-    parser.add_argument('--out_dir', '-output', type=str, default='/Users/emcmaho7/Dropbox/projects/SI_fmri/SIfMRI_analysis/data/interim')
+    parser.add_argument('--n_components', type=float, default=10)
+    parser.add_argument('--set', type=str, default='train')
+    parser.add_argument('--data_dir', '-data', type=str,
+                        default='/Users/emcmaho7/Dropbox/projects/SI_fmri/SIfMRI_analysis/data/raw')
+    parser.add_argument('--out_dir', '-output', type=str,
+                        default='/Users/emcmaho7/Dropbox/projects/SI_fmri/SIfMRI_analysis/data/interim')
     parser.add_argument('--figure_dir', '-figures', type=str,
                         default='/Users/emcmaho7/Dropbox/projects/SI_fmri/SIfMRI_analysis/reports/figures')
     args = parser.parse_args()
