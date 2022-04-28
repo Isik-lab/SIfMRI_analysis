002fde2963a61b69e61ea72cb403be02a582ecb3
emaliemcmahon@gmail.com <emaliemcmahon@gmail.com>
2022-04-28 15:37:48 -0400

    debugging

--- scripts/swurm_permutation.sh
+++ scripts/swurm_permutation.sh
@@ -1,9 +1,9 @@
 #get files
 subjs=( 1 2 3 4 )
-features=( "visual" "socialprimitive" "social" "indoor" "expanse" "agent distance" "facingness" "transitivity" "joint action" "communication" "cooperation" "dominance" "intimacy" "valence" "arousal" )
+models=( "visual" "socialprimitive" "social" "indoor" "expanse" "agent distance" "facingness" "transitivity" "joint action" "communication" "cooperation" "dominance" "intimacy" "valence" "arousal" )
 
 for subj in ${subjs[@]}; do
   for model in ${models[@]}; do
-    sbatch
+    sbatch sbatch_permutation.sh "$subj" "$model"
   done
 done
