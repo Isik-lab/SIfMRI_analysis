f7d4e468b627cd5e9737555f3e8eac45ab8491f2
emaliemcmahon@gmail.com <emaliemcmahon@gmail.com>
2022-05-01 14:16:51 -0400

    permutation testing

--- scripts/swurm_permutation.sh
+++ scripts/swurm_permutation.sh
@@ -1,7 +1,8 @@
 #get files
 subjs=( 1 2 3 4 )
 models=( "all" "visual" "socialprimitive" "social" "indoor" "expanse" "agent_distance" "facingness" "transitivity" "joint_action" "communication" "cooperation" "dominance" "intimacy" "valence" "arousal" )
-cvs=( "" "--cross_validation")
+#cvs=( " " "--cross_validation")
+cvs=( " " "")
 
 for subj in ${subjs[@]}; do
   for model in ${models[@]}; do
