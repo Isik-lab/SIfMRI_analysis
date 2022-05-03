14606fdf15d9769324aa62d118619d5a777c7868
emaliemcmahon@gmail.com <emaliemcmahon@gmail.com>
2022-05-03 13:25:28 -0400

    marcc permutation

--- scripts/swurm_permutation.sh
+++ scripts/swurm_permutation.sh
@@ -1,6 +1,6 @@
 #get files
 subjs=( 1 2 3 4 )
-models=( "all" "annotated" "nuissance" "visual" "socialprimitive" "social" )
+models=( "all" "annotated" "visual" "socialprimitive" "social" )
 
 for subj in ${subjs[@]}; do
   for model in ${models[@]}; do
