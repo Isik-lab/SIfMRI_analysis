95b1997259601054ec48f45f715e2b4baa7e0e73
emaliemcmahon@gmail.com <emaliemcmahon@gmail.com>
2022-08-05 16:15:05 -0400

    clean up

--- scripts/swurm_permutation.sh
+++
@@ -1,8 +0,0 @@
-#get files
-models=( "agent_distance" "arousal" "communication" "facingness" "joint_action"  "valence" )
-
-for subj in 1 2 3 4; do
-   for model in ${models[@]}; do
-     sbatch sbatch_permutation.sh $subj $model
-   done
-done
