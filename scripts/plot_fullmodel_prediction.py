591419657f273ff58aa615844013d6be748706c5
emaliemcmahon@gmail.com <emaliemcmahon@gmail.com>
2022-09-28 11:14:01 -0400

    increase ymax

--- scripts/plot_fullmodel_prediction.py
+++ scripts/plot_fullmodel_prediction.py
@@ -146,7 +146,7 @@ class PlotROIPrediction:
                         hue='sid', palette='gray',
                         ax=ax, data=df[df.hemi == hemi])
 
-            y_max = 0.6
+            y_max = 0.82
             ax.set_ylim([0, y_max])
 
             ax.set_xlabel('')
