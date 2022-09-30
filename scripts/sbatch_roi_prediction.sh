067563fe3ad30a304e692540dbeb254c1177d127
emaliemcmahon@gmail.com <emaliemcmahon@gmail.com>
2022-09-30 16:45:38 -0400

    roi prediction low mem

--- scripts/sbatch_roi_prediction.sh
+++ scripts/sbatch_roi_prediction.sh
@@ -4,7 +4,7 @@
 #SBATCH --job-name=fmriprep
 #SBATCH --time=25:0
 #SBATCH --partition=defq
-#SBATCH --mem=192M
+#SBATCH --mem=50M
 #SBATCH --nodes=1
 #SBATCH --mail-type=end
 #SBATCH --mail-user=emcmaho7@jhu.edu
