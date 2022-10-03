72bc3beeb09b6ae61acc268660bc5d9866fe0311
emaliemcmahon@gmail.com <emaliemcmahon@gmail.com>
2022-10-03 17:34:18 -0400

    predict responses by category

---
+++ scripts/voxel_permutation_categories.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import argparse
+import numpy as np
+from pathlib import Path
+from src import tools
+import nibabel as nib
+
+
+class CategoryVoxelPermutation:
+    def __init__(self, args):
+        self.process = 'CategoryVoxelPermutation'
+        self.sid = str(args.s_num).zfill(2)
+        self.category = args.category
+        self.step = args.step
+        self.n_perm = args.n_perm
+        self.data_dir = args.data_dir
+        self.out_dir = args.out_dir
+        self.figure_dir = f'{args.figure_dir}/{self.process}'
+        Path(f'{self.out_dir}/{self.process}').mkdir(parents=True, exist_ok=True)
+        Path(f'{self.out_dir}/{self.process}/dist').mkdir(parents=True, exist_ok=True)
+        Path(self.figure_dir).mkdir(parents=True, exist_ok=True)
+        print(vars(self))
+        im = nib.load(
+            f'{self.data_dir}/betas_3mm_zscore/sub-{self.sid}/sub-{self.sid}_space-T1w_desc-train-{self.step}_data.nii.gz')
+        self.im_shape = im.shape[:-1]
+        self.affine = im.affine
+        self.header = im.header
+        del im
+
+    def load(self):
+        in_dir = f'{self.out_dir}/CategoryVoxelRegression/'
+        pred = np.load(f'{in_dir}/sub-{self.sid}_y-pred_method-test.npy')
+        test = np.load(f'{in_dir}/sub-{self.sid}_y-test_method-test.npy')
+        return test, pred
+
+    def load_anatomy(self):
+        anat = nib.load(f'{self.data_dir}/anatomy/sub-{self.sid}/sub-{self.sid}_desc-preproc_T1w.nii.gz')
+        brain_mask = nib.load(f'{self.data_dir}/anatomy/sub-{self.sid}/sub-{self.sid}_desc-brain_mask.nii.gz')
+        return tools.mask_img(anat, brain_mask)
+
+    def nib_transform(self, r_, nii=True):
+        unmask = np.load(
+            f'{self.out_dir}/Reliability/sub-{self.sid}_space-T1w_desc-test-{self.step}_reliability-mask.npy').astype(
+            'bool')
+        i = np.where(unmask)
+        if r_.ndim < 2:
+            r_unmasked = np.zeros(unmask.shape)
+            r_unmasked[i] = r_
+            r_unmasked = r_unmasked.reshape(self.im_shape)
+        else:
+            r_ = r_.T
+            r_unmasked = np.zeros((unmask.shape + (r_.shape[-1],)))
+            r_unmasked[i, ...] = r_
+            r_unmasked = r_unmasked.reshape((self.im_shape + (r_.shape[-1],)))
+            print(r_unmasked.shape)
+
+        if nii:
+            r_unmasked = nib.Nifti1Image(r_unmasked, self.affine, self.header)
+        return r_unmasked
+
+    def save_perm_results(self, d):
+        print('Saving output')
+        for key in d.keys():
+            base = f'{self.out_dir}/{self.process}/sub-{self.sid}_category-{self.category}'
+            nib.save(d[key], f'{base}_{key}.nii.gz')
+            print(f'Saved {key} successfully')
+
+    def run(self):
+        y_true, y_pred = self.load()
+        print(np.unique(y_true))
+
+        # Run permutation
+        r2, p, r2_null = tools.perm(y_true, y_pred, n_perm=self.n_perm)
+        base = f'{self.out_dir}/{self.process}/dist/sub-{self.sid}_category-{self.category}'
+        np.save(f'{base}_r2null.npy', self.nib_transform(r2_null, nii=False))
+        del r2_null
+
+        # Run bootstrap
+        r2_var = tools.bootstrap(y_true, y_pred, n_perm=self.n_perm)
+        np.save(f'{base}_r2var.npy', self.nib_transform(r2_var, nii=False))
+        del r2_var
+
+        # filter the rs based on the significant voxels
+        r2_filtered, p_corrected = tools.filter_r(r2, p)
+
+        # transform arrays to nii
+        out_data = dict()
+        for name, i in zip(['r2', 'r2filtered', 'p', 'pcorrected'],
+                           [r2, r2_filtered, p, p_corrected]):
+            out_data[name] = self.nib_transform(i)
+        self.save_perm_results(out_data)
+        print('Completed successfully!')
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--s_num', '-s', type=int, default=1)
+    parser.add_argument('--category', type=str, default='scene_object')
+    parser.add_argument('--n_perm', type=int, default=5000)
+    parser.add_argument('--step', type=str, default='fracridge')
+    parser.add_argument('--data_dir', '-data', type=str,
+                        default='/Users/emcmaho7/Dropbox/projects/SI_fmri/SIfMRI_analysis/data/raw')
+    parser.add_argument('--out_dir', '-output', type=str,
+                        default='/Users/emcmaho7/Dropbox/projects/SI_fmri/SIfMRI_analysis/data/interim')
+    parser.add_argument('--figure_dir', '-figures', type=str,
+                        default='/Users/emcmaho7/Dropbox/projects/SI_fmri/SIfMRI_analysis/reports/figures')
+    args = parser.parse_args()
+    CategoryVoxelPermutation(args).run()
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/voxel_regression_categories.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import argparse
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import nibabel as nib
+from sklearn.linear_model import LinearRegression
+from src import tools
+
+
+def category_map(category=None):
+    d = dict()
+    d['scene_object'] = ['indoor', 'expanse', 'transitivity']
+    d['social_primitives'] = ['agent distance', 'facigness']
+    d['social'] = ['joint action', 'communication']
+    d['affective'] = ['valence', 'arousal']
+    if category is None:
+        return d
+    else:
+        return d[category]
+
+
+def scale(train_, test_):
+    mean = np.nanmean(train_, axis=0).squeeze()
+    variance = np.nanstd(train_, axis=0).squeeze()
+    variance[np.isclose(variance, 0.)] = np.nan
+    train_ = (train_ - mean) / variance
+    return train_, (test_ - mean) / variance
+
+
+def regress(X_train_, y_train_):
+    # ols
+    lr = LinearRegression(fit_intercept=False)
+    lr.fit(X_train_, y_train_)
+    return lr.coef_.T
+
+
+def predict(X_test_, betas_):
+    return X_test_ @ betas_
+
+
+def preprocess(X_train_, X_test_,
+               y_train_, y_test_):
+    X_train_, X_test_ = scale(X_train_, X_test_)
+    y_train_, y_test_ = scale(y_train_, y_test_)
+    return X_train_, X_test_, y_train_, y_test_
+
+
+class CategoryVoxelRegression:
+    def __init__(self, args):
+        self.process = 'CategoryVoxelRegression'
+        self.sid = str(args.s_num).zfill(2)
+        self.category = args.category
+        self.step = args.step
+        self.space = args.space
+        self.zscore_ses = args.zscore_ses
+        self.smooth = args.smooth
+        if self.smooth:
+            if self.zscore_ses:
+                self.beta_path = 'betas_3mm_zscore'
+            else: #self.smoothing and not self.zscore_ses:
+                self.beta_path  = 'betas_3mm_nozscore'
+        else:
+            if self.zscore_ses:
+                self.beta_path  = 'betas_0mm_zscore'
+            else: #not self.smoothing and not self.zscore_ses
+                self.beta_path  = 'betas_0mm_nozscore'
+        self.cross_validation = args.CV
+        assert not self.cross_validation, "CV not yet implemented"
+        self.n_PCs = args.n_PCs
+        self.data_dir = args.data_dir
+        self.out_dir = args.out_dir
+        Path(f'{self.out_dir}/{self.process}').mkdir(parents=True, exist_ok=True)
+        print(vars(self))
+
+    def load_neural(self, dataset):
+        mask = np.load(f'{self.out_dir}/Reliability/sub-{self.sid}_space-{self.space}_desc-test-{self.step}_reliability-mask.npy').astype('bool')
+        neural = nib.load(f'{self.data_dir}/{self.beta_path}/sub-{self.sid}/sub-{self.sid}_space-{self.space}_desc-{dataset}-{self.step}_data.nii.gz')
+        neural = np.array(neural.dataobj).reshape((-1, neural.shape[-1])).T
+        return neural[:, mask]
+
+    def load_annotated_features(self, dataset):
+        df = pd.read_csv(f'{self.data_dir}/annotations/annotations.csv')
+        set_names = pd.read_csv(f'{self.data_dir}/annotations/{dataset}.csv')
+        df = df.merge(set_names)
+        df.sort_values(by=['video_name'], inplace=True)
+        columns = category_map(self.category)
+        df = df[columns]
+        return df.to_numpy()
+
+    def load(self):
+        X_train_ = self.load_annotated_features('train')
+        X_test_ = self.load_annotated_features('test')
+        y_train_ = self.load_neural('train')
+        y_test_ = self.load_neural('test')
+        return X_train_, X_test_, y_train_, y_test_
+
+    def save_results(self, betas_, y_test_, y_pred_):
+        np.save(f'{self.out_dir}/{self.process}/sub-{self.sid}_betas_method-test.npy', betas_)
+        np.save(f'{self.out_dir}/{self.process}/sub-{self.sid}_y-test_method-test.npy', y_test_)
+        np.save(f'{self.out_dir}/{self.process}/sub-{self.sid}_y-pred_method-test.npy', y_pred_)
+
+    def train_test_regression(self, X_train_, X_test_, y_train_, y_test_):
+        X_train_, X_test_, y_train, y_test = preprocess(X_train_, X_test_, y_train_, y_test_)
+        betas = regress(X_train_, y_train_)
+        y_pred = predict(X_test_, betas)
+        self.save_results(betas, y_pred, y_test)
+
+    def run(self):
+        X_train, X_test, y_train, y_test = self.load()
+        self.train_test_regression(X_train, X_test, y_train, y_test)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--s_num', '-s', type=int, default=1)
+    parser.add_argument('--step', type=str, default='fracridge')
+    parser.add_argument('--space', type=str, default='T1w')
+    parser.add_argument('--category', type=str, default='scene_object')
+    parser.add_argument('--CV', action=argparse.BooleanOptionalAction, default=False)
+    parser.add_argument('--zscore_ses', action=argparse.BooleanOptionalAction, default=True)
+    parser.add_argument('--smooth', action=argparse.BooleanOptionalAction, default=True)
+    parser.add_argument('--n_PCs', type=int, default=8)
+    parser.add_argument('--data_dir', '-data', type=str,
+                        default='/Users/emcmaho7/Dropbox/projects/SI_fmri/SIfMRI_analysis/data/raw')
+    parser.add_argument('--out_dir', '-output', type=str,
+                        default='/Users/emcmaho7/Dropbox/projects/SI_fmri/SIfMRI_analysis/data/interim')
+    args = parser.parse_args()
+    CategoryVoxelRegression(args).run()
+
+
+if __name__ == '__main__':
+    main()
