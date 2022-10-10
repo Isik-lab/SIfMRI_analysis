122b9e1d52b1f87ca3a9f6ae2e05070d6f5af729
emaliemcmahon@gmail.com <emaliemcmahon@gmail.com>
2022-10-10 12:38:33 -0400

    rename_files.py

--- scripts/rename_files.py
+++ scripts/rename_files.py
@@ -2,6 +2,7 @@ import glob
 import shutil 
 from pathlib import Path
 
+# path = '/home/emcmaho7/scratch4-lisik3/emcmaho7/SIfMRI_analysis/data/interim/VoxelPermutation'
 path = '/home/emcmaho7/scratch4-lisik3/emcmaho7/SIfMRI_analysis/data/interim/VoxelPermutation'
 files = glob.glob(f'{path}/* *')
 for src in files:
