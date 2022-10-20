77a786316fe3136b643d8443802188c02b2e4a03
emaliemcmahon@gmail.com <emaliemcmahon@gmail.com>
2022-10-20 13:07:32 -0400

    revision of the full model

--- scripts/sbatch_permutation_full-model.sh
+++ scripts/sbatch_permutation_full-model.sh
@@ -14,12 +14,12 @@ subj=$1
 ml anaconda
 conda activate nibabel
 
-python voxel_permutation.py -s "$subj" \
+python voxel_permutation.py -s "$subj" --full_model \
   --out_dir /home/emcmaho7/scratch4-lisik3/emcmaho7/SIfMRI_analysis/data/interim \
   --data_dir /home/emcmaho7/scratch4-lisik3/emcmaho7/SIfMRI_analysis/data/raw
 
 for roi in EVC MT EBA PPA FFA LOC pSTS face-pSTS aSTS TPJ; do
-  time python roi_prediction.py -s $subj \
+  time python roi_prediction.py -s $subj --full_model \
     --roi $roi \
     --out_dir /home/emcmaho7/scratch4-lisik3/emcmaho7/SIfMRI_analysis/data/interim \
     --data_dir /home/emcmaho7/scratch4-lisik3/emcmaho7/SIfMRI_analysis/data/raw
