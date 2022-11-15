3211501b8f83d6ce991170b95ffe376f2cab1b2c
emaliemcmahon@gmail.com <emaliemcmahon@gmail.com>
2022-11-15 17:01:39 -0500

    increase n perm

--- scripts/voxel_permutation.py
+++ scripts/voxel_permutation.py
@@ -178,7 +178,7 @@ def main():
     parser.add_argument('--full_model', action=argparse.BooleanOptionalAction, default=False)
     parser.add_argument('--unique_variance', action=argparse.BooleanOptionalAction, default=False)
     parser.add_argument('--include_nuisance', action=argparse.BooleanOptionalAction, default=False)
-    parser.add_argument('--n_perm', type=int, default=5000)
+    parser.add_argument('--n_perm', type=int, default=10000)
     parser.add_argument('--step', type=str, default='fracridge')
     parser.add_argument('--data_dir', '-data', type=str,
                         default='/Users/emcmaho7/Dropbox/projects/SI_fmri/SIfMRI_analysis/data/raw')
