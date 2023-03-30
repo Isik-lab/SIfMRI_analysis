4c9551f0cc3263df0234f024f0262e915c1a04b3
emaliemcmahon@gmail.com <emaliemcmahon@gmail.com>
2023-03-30 11:49:13 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Code need to reproduce analyses in McMahon et al 2023',
+    author='Emalie McMahon',
+    license='MIT',
+)
